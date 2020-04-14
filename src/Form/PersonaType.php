152a3e039d7b64d41b29267dcfb15e6baa4653d0
Juani Alarcón <jialarcon@justiciasantafe.gov.ar>
2020-04-14 13:20:10 -0300

    Se agrega validación de solo número, max y min a campo DNI en formulario de persona.

--- src/Form/PersonaType.php
+++ src/Form/PersonaType.php
@@ -8,17 +8,18 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PersonaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dni', NumberType::class, ['label' => 'DNI', 'required' => true, 'attr' => ['autofocus' => true, 'max' => '99999999']])
-            ->add('apellido', null, ['label' => 'Apellido', 'required' => true, 'attr' => array('maxlength' => '50')])
-            ->add('nombre', null, ['label' => 'Nombre', 'required' => true, 'attr' => array('maxlength' => '50')])
-            ->add('email', EmailType::class, ['label' => 'Correo', 'required' => true, 'attr' => array('maxlength' => '80')])
-        ;
+            ->add('dni', IntegerType::class, ['label' => 'DNI', 'required' => true, 'attr' => array('autofocus' => true, 'min' => '1000000', 'max' => '99999999')])
+            ->add('apellido', TextType::class, ['label' => 'Apellido', 'required' => true, 'attr' => array('maxlength' => '50')])
+            ->add('nombre', TextType::class, ['label' => 'Nombre', 'required' => true, 'attr' => array('maxlength' => '50')])
+            ->add('email', EmailType::class, ['label' => 'Correo', 'required' => true, 'attr' => array('maxlength' => '80')]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
