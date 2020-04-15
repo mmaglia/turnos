0b8b9254342c4a8bf753f84f7d60dc9414d73fc0
Juani Alarcon <juani_alarcon@yahoo.com.ar>
2020-04-15 09:53:46 -0300

    Se fuerzan nombre y apellido de la persona a mayuscula Se corrigieron todos los botones y traducciones de los mismos en el backend Se da formato a la seleccion de fecha y hora en solitud de turno. Se mejora layout de confirmación de solictud de turno Se mejora abm de usuarios

--- src/Form/PersonaType.php
+++ src/Form/PersonaType.php
@@ -4,19 +4,20 @@ namespace App\Form;
 
 use App\Entity\Persona;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class PersonaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('dni', NumberType::class, ['label' => 'DNI', 'required' => true, 'attr' => ['autofocus' => true, 'max' => '99999999']])
-            ->add('apellido', null, ['label' => 'Apellido', 'required' => true, 'attr' => array('maxlength' => '50')])
-            ->add('nombre', null, ['label' => 'Nombre', 'required' => true, 'attr' => array('maxlength' => '50')])
+            ->add('dni', IntegerType::class, ['label' => 'DNI', 'required' => true, 'attr' => array('autofocus' => true, 'min' => '1000000', 'max' => '99999999')])
+            ->add('apellido', TextType::class, ['label' => 'Apellido', 'required' => true, 'attr' => array('maxlength' => '50')])
+            ->add('nombre', TextType::class, ['label' => 'Nombre', 'required' => true, 'attr' => array('maxlength' => '50')])
             ->add('email', EmailType::class, ['label' => 'Correo', 'required' => true, 'attr' => array('maxlength' => '80')])
         ;
     }
