3f1f64e80ee13a8b75c6bc80645ea06edaa0894a
Juani Alarcon <juani_alarcon@yahoo.com.ar>
2020-09-24 06:24:28 -0300

    Se agrega número de release (ref: tags git) al template base. Para ello se usa un parámetro PATH_SISTEMA en el archivo .env donde se pone la ruta raíz del sistema

---
+++ src/Service/UtilService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service;
+
+/**
+ * Servicio utilizado para realizar acciones generales del sistema
+ * @author jalarcon
+ */
+class UtilService
+{
+    /**
+     * @return bool|string
+     */
+    public function getGitInformation()
+    {
+        // Obtengo la raíz donde corre el sistema
+        $path = $_ENV['PATH_SISTEMA'];
+
+        // Si no es un directorio devuelvo false
+        if (!\is_dir($path . DIRECTORY_SEPARATOR . '.git')) {
+            return false;
+        }
+
+        // Comando git a ejecutar
+        $process = \proc_open(
+            'git describe --tags',
+            [
+                1 => ['pipe', 'w'],
+                2 => ['pipe', 'w'],
+            ],
+            $pipes,
+            $path
+        );
+
+        if (!\is_resource($process)) {
+            return false;
+        }
+
+        // Ejecuto el comando y lo guardo en los pipes
+        $result = \trim(\stream_get_contents($pipes[1]));
+
+        // Cierrro los pipes
+        \fclose($pipes[1]);
+        \fclose($pipes[2]);
+
+        // Obtengo el retorno
+        $returnCode = \proc_close($process);
+
+        if ($returnCode !== 0) {
+            return false;
+        }
+
+        return $result;
+    }
+}
